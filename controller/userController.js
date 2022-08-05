a7cf748fa58d3908d76c537610c70badf4be597b
demershow <demersontorres520@gmail.com>
2022-08-05 12:16:57 -0300

    fix: using spreend instead of array

--- controller/userController.js
+++ controller/userController.js
@@ -1,19 +1,21 @@
 class UserController {
 
-    constructor(formId) {
+    constructor(formId, tableId) {
 
-        this.formEl = docmuent.getElementbyId(formId);
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+
+        this.onSubmit();
 
     }
 
-    onSubmit(){
+    onSubmit() {
 
         this.formEl.addEventListener('submit', (event) => {
 
             event.preventDefault();
 
-            this.getValues();
-        
+            this.addLine(this.getValues());
         })
 
     }
@@ -22,7 +24,7 @@ class UserController {
 
         let user = {};
 
-        this.formEl.elements.forEach(function (field, index) {
+        [...this.formEl.elements].forEach(function (field, index) {
 
             if (field.name == "gender") {
 
@@ -47,11 +49,35 @@ class UserController {
             user.email,
             user.passowrd,
             user.photo,
-            user.admin);
+            user.admin
+        );
 
 
     }
+
+    addLine(dataUser,) {
+
+        this.tableEl.innerHTML = `
+                <tr>
+                    <td>
+                      <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+                    </td>
+                    <td>${dataUser.name}</td>
+                    <td>${dataUser.email}r</td>
+                    <td>${dataUser.admin}</td>
+                    <td>${dataUser.data}</td>
+                    <td>
+                      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    </td>
+                </tr>
     
+    `;
+
+
+
+    }
+
 
 }
 
