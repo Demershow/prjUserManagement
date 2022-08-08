312f67dca15cdb968f95d7d2968684454caa8991
demershow <demersontorres520@gmail.com>
2022-08-08 08:58:23 -0300

    fix: img error

--- controller/userController.js
+++ controller/userController.js
@@ -1,92 +1,128 @@
 class UserController {
 
-
-
-    //Método construtor
     constructor(formId, tableId) {
 
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
 
-        this.onSubmit();
+        this.onSubmit()
 
     }
 
-
-    //Método no momento do click para a validação
     onSubmit() {
 
-        this.formEl.addEventListener('submit', (event) => {
+        this.formEl.addEventListener("submit", event => {
 
             event.preventDefault();
 
-            this.addLine(this.getValues());
-        })
+            let values = this.getValues();
+
+            this.getPhoto().then(
+                 (content)=> {
+
+
+                    values.photo = content;
+
+                    this.addLine(values);
+
+                },
+                 (e) => {
+                    console.error
+                }
+            );
+
+        });
 
     }
 
+    getPhoto(formEl) {
+        return new Promise((resolve, reject) => {
+            let fileReader = new FileReader();
 
-    
+            let elements = [...formEl.elements].filter(item => {
+                if (item.name === 'photo') {
+                    return item;
+                }
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+
+            fileReader.onerror = (e) => {
+                reject(e);
+            }
+
+            if (file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
+        });
+    }
+
+
     getValues() {
 
         let user = {};
 
         [...this.formEl.elements].forEach(function (field, index) {
 
-            if (field.name == "gender") {
+            if (field.name === "gender") {
 
                 if (field.checked) {
-                    user[field.name] = field.value;
-
+                    user[field.name] = field.value
                 }
-            } else {
 
-                user[field.name] = field.value;
+            } else if(field.name =="admin") {
+
+                user[field.name] = field.checked;
+
+            } else{
+
+                user[field.name] = field.value
 
             }
 
         });
 
-        //Retorno do usuario com os dados inseridos
         return new User(
             user.name,
             user.gender,
             user.birth,
             user.country,
             user.email,
-            user.passowrd,
+            user.password,
             user.photo,
             user.admin
         );
 
-
     }
 
 
-    //Método para adicionar a linha inserida ao sistema
-    addLine(dataUser,) {
+    addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-                <tr>
-                    <td>
-                      <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
-                    </td>
-                    <td>${dataUser.name}</td>
-                    <td>${dataUser.email}r</td>
-                    <td>${dataUser.admin}</td>
-                    <td>${dataUser.data}</td>
-                    <td>
-                      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                    </td>
-                </tr>
-    
-    `;
+        let tr = document.createElement('tr');
 
+        tr.innerHTML = `
+            <tr>
+                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${(dataUser.admin) ? 'sim' : 'não'}</td>
+                <td>${dataUser.birth}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr>
+        `;
 
+        this.tableEl.appendChild(tr);
 
     }
 
 
 }
-
